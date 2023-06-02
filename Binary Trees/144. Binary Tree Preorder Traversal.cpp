ac14c8ea0bfa93a98073a348962ba35d2418dd93
Aaditry Choudhury <aaditrychoudhury@gmail.com>
2023-06-02 14:31:43 +0530

    Update 144. Binary Tree Preorder Traversal.cpp

--- Binary Trees/144. Binary Tree Preorder Traversal.cpp
+++ Binary Trees/144. Binary Tree Preorder Traversal.cpp
@@ -12,14 +12,15 @@
 class Solution {
 public:
     vector<int> ans;
-    void preorder(TreeNode* root){
-        ans.push_back(root->val);
-        if(root->left) preorder(root->left);
-        if(root->right) preorder(root->right);
-    }
     vector<int> preorderTraversal(TreeNode* root) {
-        if(root == NULL) return ans;
         preorder(root);
         return ans;
+    }
+    void preorder(TreeNode* root){
+        if(root != NULL){
+            ans.push_back(root->val);
+            preorder(root->left);
+            preorder(root->right);
+        }
     }
 };
