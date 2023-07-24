423c0989d3579a8343968eb3d6baa7ca9bcf7588
Aaditry Choudhury <aaditrychoudhury@gmail.com>
2023-07-24 23:01:50 +0530

    Create Subarray with given XOR.cpp

---
+++ SDE SHEET/Day4/Subarray with given XOR.cpp
@@ -0,0 +1,13 @@
+int Solution::solve(vector<int> &A, int B) {
+    int xr =0;
+    map<int,int> mp;
+    mp[xr]++;
+    int cnt=0;
+    for(int i=0;i<A.size();++i){
+        xr = xr ^ A[i];
+        int x = xr^B;
+        cnt+= mp[x];
+        mp[xr]++;
+    }
+    return cnt;
+}
